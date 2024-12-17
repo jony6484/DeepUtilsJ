8ef3e3895933fffd99b806bade60d2d7b61eca5f
Jonathan Fuchs <jony6484@gmail.com>
2024-12-17 17:10:09 +0200

    project skeleton

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='DeepUtils',
+    packages=find_packages(include=['DeepUtils']),
+    version='0.1.0',
+    description='Deep learning utils',
+    author='Jonathan Fuchs',
+)
