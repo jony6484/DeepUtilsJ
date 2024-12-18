e368466d4e52e149ff86c78a7ba5489572e3b14b
Jonathan Fuchs <jony6484@gmail.com>
2024-12-18 11:08:19 +0200

    rename

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='DeepUtils',
-    packages=find_packages(include=['DeepUtils']),
+    name='DeepUtilsJ',
+    packages=find_packages(include=['DeepUtilsJ']),
     version='0.1.0',
     description='Deep learning utils',
     author='Jonathan Fuchs',
